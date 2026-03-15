4aaed77957dd8a449293ba5a5a1e388f2f8f8d70
smanaman <sanvatsman@gmail.com>
2026-03-15 16:36:12 +0530

    dbchange

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/projectmaker");
+mongoose.connect("mongodb+srv://sanvatsman_db_user:6X0rikFfjvWN0PWg@aiprojectmaker.ynfinkv.mongodb.net/aiprojetmaker?appName=AIprojectmaker");
 
 const db = mongoose.connection;
 
